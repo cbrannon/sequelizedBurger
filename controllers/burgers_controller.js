c681f0904707fc00b8d2c3864ef6f118374449c7
Cody Brannon <codybrannon@Codys-MacBook-Pro.local>
2017-05-08 20:47:03 -0400

    Swapped orm for sequelize

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,13 +1,11 @@
 var express = require("express");
+var db = require("../models");
 
 var router = express.Router();
 
-// Import the model (burger.js) to use its database functions.
-var burger = require("../models/burger.js");
-
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
-  burger.all(function(data) {
+  db.Burger.findAll({}).then(function(data) {
     var hbsObject = {
       burgers: data
     };
@@ -17,23 +15,21 @@ router.get("/", function(req, res) {
 });
 
 router.post("/", function(req, res) {
-  burger.create([
-    "burger_name"
-  ], [
-    req.body.burger_name
-  ], function() {
+  db.Burger.create({
+    burger_name: req.body.burger_name
+  }).then(function() {
     res.redirect("/");
   });
 });
 
 router.put("/:id", function(req, res) {
-  var condition = "id = " + req.params.id;
-
-  console.log("condition", condition);
-
-  burger.update({
+  db.Burger.update({
     devoured: true
-  }, condition, function() {
+  }, {
+    where: {
+      id: req.params.id
+    }
+  }).then(function() {
     res.redirect("/");
   });
 });
